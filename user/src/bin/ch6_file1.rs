c7610210e1728521784fd7aa2e9707fb956af12b
zondayc <2645828175@qq.com>
2022-12-08 09:35:15 +0800

    finish link unlink fstat

--- user/src/bin/ch6_file1.rs
+++ user/src/bin/ch6_file1.rs
@@ -3,6 +3,8 @@
 
 #[macro_use]
 extern crate user_lib;
+use core::prelude;
+
 use user_lib::{close, fstat, open, OpenFlags, Stat, StatMode};
 
 /// 测试 fstat，输出　Test fstat OK! 就算正确。
@@ -14,6 +16,7 @@ pub fn main() -> i32 {
     assert!(fd > 0);
     let fd = fd as usize;
     let stat: Stat = Stat::new();
+    //println!("begin fstat");
     let ret = fstat(fd, &stat);
     assert_eq!(ret, 0);
     assert_eq!(stat.mode, StatMode::FILE);
